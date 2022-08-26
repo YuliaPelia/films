36ffdf2386bf921474c9fab21ff5258c811e41a0
Yulia <yuliapelia18@gmail.com>
2022-08-26 09:48:48 +0300

    css file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberOfFilms = +prompt("Скільки фільмів ви вже переглянули?", "");
+const numberOfFilms = prompt("Скільки фільмів ви вже переглянули?", "");
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,12 +9,36 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt('Один з останніх переглянутих фільмів?', ''),
-    b = prompt('На скільки оціните його?'),
-    c = prompt ('Один з останніх переглянутих фільмів?', ''),
-    d = prompt('На скільки оціните його?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один з останніх переглянутих фільмів?', ''),
+          b = prompt('На скільки оціните його?');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+          
+  
+}
+
+
+
+
+
+if(personalMovieDB.count < 10) {
+    console.log("Переглянуто мало фільмів");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Ви класичний глядач");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Ви кіноман");
+} else {
+    console.log("Помилка");
+}
 
 console.log(personalMovieDB);
+
